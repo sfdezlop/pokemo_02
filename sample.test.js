b78aec1df331753e6da7f3211be49b82e7830f75
sfernandez <sfdezlop@gmail.com>
2023-02-05 13:16:32 +0100

    Solve Netlify Bug

--- sample.test.js
+++
@@ -1,9 +0,0 @@
-import { sample } from './sample';
-describe('Given a sample array', () => {
-    test('When the index is 0 the element is "Hola Don José"', () => {
-        const r = sample[0];
-        expect(r).toBe('Hola Don José');
-        const result = sample[1];
-        expect(result).toBe('2');
-    });
-});
