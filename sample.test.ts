4f6076aac89cf3dc8bc717f77f55939e08882f29
sfernandez <sfdezlop@gmail.com>
2023-02-05 03:00:23 +0100

    Check local coverage

--- sample.test.ts
+++ sample.test.ts
@@ -4,5 +4,7 @@ describe('Given a sample array', () => {
   test('When the index is 0 the element is "Hola Don José"', () => {
     const r: string = sample[0];
     expect(r).toBe('Hola Don José');
+    const result: string = sample[1];
+    expect(result).toBe('2');
   });
 });
